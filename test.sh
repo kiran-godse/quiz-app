daa45ae498f09521c5d99b269558b4bef85e1acb
swapnil <swapnilgawali244@gmail.com>
2023-06-21 12:22:46 +0530

    set new workflow

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-jq 
+json_content=$(jq -c . < ./recipe/cmake/recipe.json)
+package_kind=$(echo "$json_content" | jq .package.kind)
+package_name=$(echo "$json_content" | jq .package.name)
+
+
+echo "$package_kind"
+echo "$package_name"
